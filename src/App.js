841a29f4da778b7c3e0f0cb1988e3e6270cdb3e0
OLAGUNJU Oladele Emmanuel <149441709+MLO-OLAGUNJU@users.noreply.github.com>
2024-02-08 17:53:06 +0100

    Building a Netflix full clone project

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import NavBar from "./components/NavBar";
+import Home from "./pages/Home";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <>
       <NavBar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
     </>
   );
 }
