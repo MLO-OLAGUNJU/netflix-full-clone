9cc92b275573e14831ad10139bb1d31546c5ff4c
OLAGUNJU Oladele Emmanuel <149441709+MLO-OLAGUNJU@users.noreply.github.com>
2024-02-08 17:43:39 +0100

    Building a Netflix full clone project

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -2,12 +2,15 @@ import React from "react";
 
 const NavBar = () => {
   return (
-    <div className="text-white">
-      <h1>NETFLIX</h1>
-
+    <div className="flex items-center justify-between p-4 z-[100] w-full absolute">
+      <h1 className="text-red-600 text-4xl font-bold cursor-pointer">
+        NETFLIX
+      </h1>
       <div>
-        <button>Sign In</button>
-        <button>Sign Up</button>
+        <button className="text-white pr-4">Sign In</button>
+        <button className="bg-red-600 px-6 py-2 rounded cursor-pointer text-white">
+          Sign Up
+        </button>
       </div>
     </div>
   );
