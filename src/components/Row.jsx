db17fc45e1e08ab83201d031b3ed128e0fb81f7c
OLAGUNJU Oladele Emmanuel <149441709+MLO-OLAGUNJU@users.noreply.github.com>
2024-02-08 21:29:35 +0100

    Building a Netflix full clone project

--- src/components/Row.jsx
+++ src/components/Row.jsx
@@ -16,7 +16,10 @@ const Row = ({ title, fetchURL }) => {
     <>
       <h2 className="text-white font-bold md:text-xl p-4">{title}</h2>
       <div className="relative flex items-center">
-        <div id={"slider"}>
+        <div
+          id={"slider"}
+          className="w-full h-full overflow-x-scroll whitespace-nowrap scroll-smooth scrollbar-hide"
+        >
           {movies.map((item, id) => (
             <Movie item={item} key={id} />
           ))}
