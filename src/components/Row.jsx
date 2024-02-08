0dbde190763a78866fc318ff7467274724de5175
OLAGUNJU Oladele Emmanuel <149441709+MLO-OLAGUNJU@users.noreply.github.com>
2024-02-08 19:46:33 +0100

    Building a Netflix full clone project

---
+++ src/components/Row.jsx
@@ -0,0 +1,22 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+
+const Row = ({ title, fetchURL }) => {
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    axios.get(fetchURL).then((response) => {
+      setMovies(response.data.results);
+    });
+  }, [fetchURL]);
+  return (
+    <>
+      <h2 className="text-white font-bold md:text-xl p-4">{title}</h2>
+      <div className="relative flex items-center">
+        <div id={"slider"}></div>
+      </div>
+    </>
+  );
+};
+
+export default Row;
