1ec9d2edff5e4513450cb9824f084db2123b503c
OLAGUNJU Oladele Emmanuel <149441709+MLO-OLAGUNJU@users.noreply.github.com>
2024-02-08 22:29:57 +0100

    Building a Netflix full clone project

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import NavBar from "./components/NavBar";
+import { AuthContextProvider } from "./context/AuthContext";
 import Home from "./pages/Home";
 import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <>
-      <NavBar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-      </Routes>
+      <AuthContextProvider>
+        <NavBar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </AuthContextProvider>
     </>
   );
 }
