6b1ef8a1eeda067b75c4dd77f5f99b6850c2454d
OLAGUNJU Oladele Emmanuel <149441709+MLO-OLAGUNJU@users.noreply.github.com>
2024-02-09 22:03:59 +0100

    I updated my  Netflix full clone project

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -14,9 +14,9 @@ export function AuthContextProvider({ children }) {
   const [user, setUser] = useState({});
 
   function signUp(email, password) {
-    // createUserWithEmailAndPassword(auth, email, password)
+    createUserWithEmailAndPassword(auth, email, password)
       .then((userCredential) => {
-        User created successfully, now set user data in Firestore
+        // User created successfully, now set user data in Firestore
         setDoc(doc(db, "users", email), {
           savedShows: [],
         });
