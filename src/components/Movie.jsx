74ea061cccade6df08dec7178a4a2c2785b3e30c
OLAGUNJU Oladele Emmanuel <149441709+MLO-OLAGUNJU@users.noreply.github.com>
2024-02-09 11:16:53 +0100

    Building a Netflix full clone project

--- src/components/Movie.jsx
+++ src/components/Movie.jsx
@@ -10,7 +10,23 @@ const Movie = ({ item }) => {
   const { user } = UserAuth;
   const [saved, setSaved] = useState(false);
 
-  const movieId = doc(db, "users", `${user?.email}`);
+  const movieID = doc(db, "users", `${user?.email}`);
+
+  const saveShow = async () => {
+    if (user?.email) {
+      setLike(!like);
+      setSaved(true);
+      await updateDoc(movieID, {
+        savedShows: arrayUnion({
+          id: item.id,
+          title: item.title,
+          img: item.backdrop_path,
+        }),
+      });
+    } else {
+      alert("Please log in to save a movie");
+    }
+  };
 
   return (
     <div className=" w-[160px] sm:w-[200px] lg:w-[200px] inline-block cursor-pointer relative p-2">
@@ -23,7 +39,7 @@ const Movie = ({ item }) => {
         <p className="white-space-normal text-xs md:text-sm font-bold flex justify-center items-center h-full text-center text-wrap">
           {item?.title}
         </p>
-        <p>
+        <p onClick={saveShow}>
           {like ? (
             <FaHeart className="absolute top-4 left-4 text-gray-300" />
           ) : (
