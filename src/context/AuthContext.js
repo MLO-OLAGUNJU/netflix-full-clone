ab143fbd744e316989cc7111ea3f7c0fb51e29af
OLAGUNJU Oladele Emmanuel <149441709+MLO-OLAGUNJU@users.noreply.github.com>
2024-02-09 22:06:25 +0100

    I updated my  Netflix full clone project

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -1,4 +1,4 @@
-// import { createContext, useContext, useEffect, useState } from "react";
+import { createContext, useContext, useEffect, useState } from "react";
 import { auth, db } from "../firebase";
 import {
   createUserWithEmailAndPassword,
